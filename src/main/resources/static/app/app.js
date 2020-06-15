3c4a62253651c31be35e16957825c7383d7e2121
ChizhStanislav <stas23041991@gmail.com>
2020-06-16 00:42:32 +0300

    add

--- src/main/resources/static/app/app.js
+++ src/main/resources/static/app/app.js
@@ -4,18 +4,32 @@ import {BrowserRouter} from "react-router-dom";
 import s from './App.module.scss'
 import Footer from "./components/footerBlock/footer";
 import Body from "./components/bodyBlock/body";
+import ReactDOM from "react-dom";
+import store from "./data/state";
 
 
-function App(props) {
-    return (
-        <BrowserRouter>
-            <div className={s.app}>
-                <Header menu={props.store.getState().links}/>
-                <Body data={props.store.getState()}/>
-                <Footer/>
-            </div>
-        </BrowserRouter>
-    );
+class App extends React.Component {
+
+    render() {
+        return <p>Hello, World!</p>
+    }
 }
 
-export default App;
+
+class App extends React.Component {
+
+    render(){
+        return (
+            <BrowserRouter>
+                <div className={s.app}>
+                    <Header menu={store.getState().links}/>
+                    <Body data={store.getState()}/>
+                    <Footer/>
+                </div>
+            </BrowserRouter>
+        );
+    }
+
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/main/resources/static/app/index.js
+++ src/main/resources/static/app/index.js
@@ -1,13 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
+import App from './app/app.jsx'
 import './index.css';
-import store from "./data/state";
 
-let renderEntireTree = (store) => {
-    ReactDOM.render(<App store={store} />, document.getElementById('root'));
-};
 
-renderEntireTree(store);
+
+
+
+
 
 
